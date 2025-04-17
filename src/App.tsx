ee6e1ce1a8bd21f0ab88e126112c725c3ddc14f4
JJtheJJpro <jjthejjpro@gmail.com>
2025-04-16 23:55:28 -0600

    upload idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { List, ListItem, ListItemContent, Stack, Tab, TabList, Tabs } from '@mui/joy'
+import { Button, DialogContent, DialogTitle, List, ListItem, ListItemContent, Modal, ModalClose, ModalDialog, Stack, Tab, TabList, Tabs, Typography } from '@mui/joy'
 import './App.css'
 import '@mui/joy'
 import { useState } from 'react'
 import { GrLike, GrLikeFill } from 'react-icons/gr'
+import { encode } from 'base-64'
 
-function VoteImage(props: { image?: ImageBitmap }) {
+function VoteImage(props: { image?: Uint8Array }) {
+    const str = String.fromCharCode(...props.image ?? [0]);
+    const img = props.image ? encode(str) : '';
     return (
         <div className='voteimage'>
             <div className='image'>
-                <p>image here</p>
+                <img src={img} title='image not found'></img>
             </div>
             <div className='vote'>
                 <GrLike stroke='green' style={{ padding: "10px", width: "20px", height: "20px" }} />
@@ -40,10 +43,41 @@ function VoteImageList(props: { year: number }) {
 
 function App() {
     const [year, setYear] = useState(2025);
+    const [upload, setUpload] = useState(false);
+    const [imageFile, setImageFile] = useState<string | undefined>(undefined);
+    const [_uploadYear, setUploadYear] = useState(2025);
+    const [uploadGood, setUploadGood] = useState(false);
 
     return (
         <>
             <div className='topbar'>
+                <Button color='primary' onClick={() => setUpload(true)}>Upload</Button>
+                <Modal open={upload} onClose={() => {
+                    setUpload(false);
+                    setUploadGood(false);
+                    setImageFile(undefined);
+                    setUploadYear(2025);
+                }}>
+                    <ModalDialog variant='outlined'>
+                        <ModalClose />
+                        <DialogTitle>Upload a photo</DialogTitle>
+                        <DialogContent>Upload a crazy photo of hair of an athlete.</DialogContent>
+                        <input type='file' title='Image' accept='image/*' onChange={async e => {
+                            setUploadGood(e.target.files !== null);
+                            if (e.target.files !== null) {
+                                setImageFile(URL.createObjectURL(new Blob([await e.target.files[0].arrayBuffer()])));
+                            } else {
+                                setImageFile(undefined);
+                            }
+                        }} />
+                        <label htmlFor='selectyear'>Year</label>
+                        <select id='selectyear' name='year' title='Year'>
+                            {[...Array(26)].map((_, i) => <option key={i} value={2025 - i} onSelect={() => setUploadYear(2025 - i)}>{2025 - i}</option>)}
+                        </select>
+                        {imageFile && <img title='Image' src={imageFile} className='upload'></img>}
+                        <Button color='success' disabled={!uploadGood}>Upload</Button>
+                    </ModalDialog>
+                </Modal>
                 <Stack sx={{ width: '100%' }}>
                     <Tabs>
                         <TabList tabFlex={1}>
